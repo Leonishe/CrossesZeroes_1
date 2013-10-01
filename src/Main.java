d0757650c7a5b310df19686caf82f4ebc309814b
Leonishe <beloozersk@hotmail.com>
2013-10-01 12:37:32 +0400

    Changes

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,42 @@ public class Main {
 
     public static void main(String[] args) {
         Players players = new Players();
-        Units units = new Units();
         players.Names();
         players.NameOut();
+        Units units =new Units();
         units.defaultUnit();
         units.newUnitOut();
+
         for (int i=1;i<10;i++){
             if (i%2==0){
-              players.Names();
+
+                units.startNewUnit(i,players.player2);
+                units.newUnitOut();
             }
+            else {
+                units.startNewUnit1(i,players.player1);
+                units.newUnitOut();
+            }
+                if (i>=5){
+                    int game = units.checkWinner();
+                    if (game==1){
+                        if (i%2==0){
+                            System.out.println(players.player2 + " WON!");
+                            break;
+                        }
+                        else {
+                            System.out.println(players.player1 + " WON!");
+                            break;
+                        }
+
+                    }
+                    else
+                        if (game==0){
+                        continue;
+                    }
+
+                }
+
         }
 
     }
--- src/Units.java
+++ src/Units.java
@@ -1,18 +1,11 @@
 import java.util.Scanner;
 
-public class Units {
+public class Units extends Players{
     String[][] units = new String[3][3];
     public String unit;
 
-    public String getUnit(){
-        return unit;
-    }
 
-    public void setUnit(String unit){
-        if (unit != "X" | unit != "0")
-        this.unit = unit;
-        else return;
-    }
+
 
     public void defaultUnit(){
 
@@ -35,10 +28,19 @@ public class Units {
 
     public int getStarted(String player){
         int item = 0;
+              System.out.println(player + ", введите номер поля");
+              Scanner newItem = new Scanner(System.in);
+              item = Integer.parseInt(newItem.next());
+                    if((item<0)|(item>9))
+                            do {
+                             System.out.println(item + "-й бокс:");
+                             System.out.println("Введите номер ещё раз:");
+                             item = Integer.parseInt(newItem.next());
+                    }  while ((item<0)|(item>9));
 
-                System.out.println(player + "Введите номер поля");
-                Scanner newItem = new Scanner(System.in);
-                item = Integer.parseInt(newItem.next());
+
+
+
 
         return(item);
     }
@@ -46,16 +48,16 @@ public class Units {
     public int checkWinner(){
         int i =0;
             if(
-               ((units[1][1] == units[1][2]) & (units[1][1] == units[1][3]) & (units[1][2] == units[1][3]))|
-               ((units[2][1] == units[2][2]) & (units[2][1] == units[2][3]) & (units[2][2] == units[2][3]))|
-               ((units[3][1] == units[3][2]) & (units[3][1] == units[3][3]) & (units[3][2] == units[3][3]))|
+               ((units[0][0] == units[0][1]) & (units[0][0] == units[0][2]) & (units[0][1] == units[0][2]))|
+               ((units[1][0] == units[1][1]) & (units[1][0] == units[1][2]) & (units[1][1] == units[1][2]))|
+               ((units[2][0] == units[2][1]) & (units[2][0] == units[2][2]) & (units[2][1] == units[2][2]))|
 
-               ((units[1][1] == units[2][1]) & (units[1][1] == units[3][1]) & (units[2][1] == units[3][1]))|
-               ((units[1][2] == units[2][2]) & (units[1][2] == units[3][2]) & (units[2][2] == units[3][2]))|
-               ((units[1][3] == units[2][3]) & (units[1][3] == units[3][3]) & (units[3][2] == units[3][3]))|
+               ((units[0][0] == units[1][0]) & (units[0][0] == units[2][0]) & (units[1][0] == units[2][0]))|
+               ((units[0][1] == units[1][1]) & (units[0][1] == units[2][1]) & (units[1][1] == units[2][1]))|
+               ((units[0][2] == units[1][2]) & (units[0][2] == units[2][2]) & (units[1][2] == units[2][2]))|
 
-               ((units[1][1] == units[2][2]) & (units[1][1] == units[3][3]) & (units[2][2] == units[3][3]))|
-               ((units[1][3] == units[2][2]) & (units[1][3] == units[3][1]) & (units[2][2] == units[3][1]))
+               ((units[0][0] == units[1][1]) & (units[1][1] == units[2][2]) & (units[0][0] == units[2][2]))|
+               ((units[0][2] == units[1][1]) & (units[0][2] == units[2][0]) & (units[1][1] == units[2][0]))
 
                ){
                  i=1;
@@ -63,15 +65,52 @@ public class Units {
         return(i);
     }
 
-    public void startNewUnit(){
-        for (int i=1; i<10; i++)
-            if (i%2==0) {
-                String player = Players
-                switch (getStarted()){
-            }
+    public void startNewUnit(int i, String player){
 
 
-        }
+                switch ( getStarted(player)){
+                    case 1: units[0][0]="0";
+                        break;
+                    case 2: units[0][1]="0";
+                        break;
+                    case 3: units[0][2]="0";
+                        break;
+                    case 4: units[1][0]="0";
+                        break;
+                    case 5: units[1][1]="0";
+                        break;
+                    case 6: units[1][2]="0";
+                        break;
+                    case 7: units[2][0]="0";
+                        break;
+                    case 8: units[2][1]="0";
+                        break;
+                    case 9: units[2][2]="0";
+                        break;
+                }
+    }
+    public void startNewUnit1(int i, String player){
+               switch (getStarted(player)){
+                   case 1: units[0][0]="X";
+                        break;
+                   case 2: units[0][1]="X";
+                        break;
+                   case 3: units[0][2]="X";
+                        break;
+                   case 4: units[1][0]="X";
+                        break;
+                   case 5: units[1][1]="X";
+                        break;
+                   case 6: units[1][2]="X";
+                        break;
+                   case 7: units[2][0]="X";
+                        break;
+                   case 8: units[2][1]="X";
+                        break;
+                   case 9: units[2][2]="X";
+                        break;
+               }
+
     }
 
 }
